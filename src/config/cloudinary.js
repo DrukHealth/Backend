9caab9102e88328db49549030814453a4141c583
Kunzang Cheki <kcheki46@gmail.com>
2025-12-02 15:10:24 +0600

    cors update

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,21 +1,19 @@
+// src/config/cloudinary.js
 import dotenv from "dotenv";
 dotenv.config();
 
-import { v2 as cloudinary } from "cloudinary";
-import CloudinaryStoragePkg from "multer-storage-cloudinary";
+import cloudinary from "cloudinary";                // default import (package)
+import pkg from "multer-storage-cloudinary";       // CommonJS pkg
+const CloudinaryStorage = pkg.CloudinaryStorage ? pkg.CloudinaryStorage : pkg;
 
-const CloudinaryStorage = CloudinaryStoragePkg.CloudinaryStorage
-  ? CloudinaryStoragePkg.CloudinaryStorage
-  : CloudinaryStoragePkg;
-
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  api_key:    process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-  cloudinary,
+  cloudinary,   // pass the package object (has .v2)
   params: {
     folder: "drukhealth_ctg_uploads",
     allowed_formats: ["jpg", "jpeg", "png"],
